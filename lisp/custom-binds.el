1d930e2581c3d13908f052ed15d58ff287da2f45
Reberti Carvalho Soares <6reberti6@gmail.com>
2019-10-20 17:27:11 -0300

    Add: Everything back togheter
    And faster

--- lisp/custom-binds.el
+++ lisp/custom-binds.el
@@ -19,20 +19,15 @@
 	(other-window 1))
 
 ;; Config
-(defun config-visit(file)
+(defun config-visit()
 	"Open Config Files"
-	(interactive "sWhich File? ")
-	(if (= (length file) 0)
-		(find-file "~/.emacs.d/config/general.org")
-		(find-file (concat "~/.emacs.d/config/" file ".org"))))
+	(interactive)
+	(find-file "~/.emacs.d/config.org"))
 
 (defun config-reload ()
 	"Reload Config Files"
 	(interactive)
-	(org-babel-load-file (expand-file-name "~/.emacs.d/config/general.org"))
-	(org-babel-load-file (expand-file-name "~/.emacs.d/config/visual.org"))
-	(org-babel-load-file (expand-file-name "~/.emacs.d/config/keybindings.org"))
-	(org-babel-load-file (expand-file-name "~/.emacs.d/config/packages.org")))
+	(org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
 ;; Buffers
 (defun kill-current-buffer ()
@@ -42,3 +37,14 @@
 (defun kill-all-buffers ()
    (interactive)
    (mapc 'kill-buffer (buffer-list)))
+
+;; Email
+;"rm $(notmuch search --output=files --exclude=false tag:deleted)"
+(defun notmuch-delete-tagged ()
+  "Delete emails tagged with deleted"
+  (interactive)
+  (shell-command "notmuch search --output=files --exclude=false tag:deleted > email_list")
+  (shell-command "xargs rm < email_list")
+  (message "%s Emails removed" (shell-command "wc ./email_list -l"))
+  (shell-command "rm email_list")
+  (shell-command "notmuch new"))
