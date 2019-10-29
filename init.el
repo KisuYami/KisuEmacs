e89d11069c4b35b3f7f516c909db0a791c3a6b91
Reberti Carvalho Soares <6reberti6@gmail.com>
2019-10-29 07:50:42 -0300

    Removed more harmfull stuff

--- init.el
+++ init.el
@@ -52,10 +52,13 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
+ '(custom-safe-themes
+   (quote
+	("f951343d4bbe5a90dba0f058de8317ca58a6822faa65d8463b0e751a07ec887c" "2a3ffb7775b2fe3643b179f2046493891b0d1153e57ec74bbe69580b951699ca" "071f5702a5445970105be9456a48423a87b8b9cfa4b1f76d15699b29123fb7d8" "332e009a832c4d18d92b3a9440671873187ca5b73c2a42fbd4fc67ecf0379b8c" default)))
  '(delete-selection-mode nil)
  '(package-selected-packages
    (quote
-	(love-minor-mode auto-complete slime-company slime company-jedi zzz-to-char rainbow-delimiters avy ivy projectile sunrise-x-modeline sunrise-x-buttons sunrise-commander twittering-mode zerodark-theme pretty-mode flycheck-clang-analyzer flycheck-irony flycheck yasnippet-snippets yasnippet company-c-headers company-shell company-irony irony irony-mode company-lua mark-multiple expand-region swiper popup-kill-ring dmenu ido-vertical-mode ido-vertical ox-html5slide centered-window-mode htmlize ox-twbs diminish erc-hl-nicks symon rainbow-mode switch-window dashboard smex company sudo-edit emms magit org-bullets hungry-delete beacon linum-relative spaceline fancy-battery exwm which-key use-package)))
+	(org-beautify-theme dracula-theme doom-modeline doom-themes love-minor-mode auto-complete slime-company slime company-jedi zzz-to-char rainbow-delimiters avy ivy projectile sunrise-x-modeline sunrise-x-buttons sunrise-commander twittering-mode zerodark-theme pretty-mode flycheck-clang-analyzer flycheck-irony flycheck yasnippet-snippets yasnippet company-c-headers company-shell company-irony irony irony-mode company-lua mark-multiple expand-region swiper popup-kill-ring dmenu ido-vertical-mode ido-vertical ox-html5slide centered-window-mode htmlize ox-twbs diminish erc-hl-nicks symon rainbow-mode switch-window dashboard smex company sudo-edit emms magit org-bullets hungry-delete beacon linum-relative spaceline fancy-battery exwm which-key use-package)))
  '(pos-tip-background-color "#36473A")
  '(pos-tip-foreground-color "#FFFFC8"))
 (custom-set-faces
--- lisp/custom-binds.el
+++ lisp/custom-binds.el
@@ -3,31 +3,31 @@
   :group 'convenience
   :prefix "custom-binds-")
 
- (provide 'custom-binds)
+(provide 'custom-binds)
 
 ;; Window Bindings
 (defun split-and-fallow-h ()
-	(interactive)
-	(split-window-below)
-	(balance-windows)
-	(other-window 1))
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (other-window 1))
 
 (defun split-and-fallow-v ()
-	(interactive)
-	(split-window-right)
-	(balance-windows)
-	(other-window 1))
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
 
 ;; Config
 (defun config-visit()
-	"Open Config Files"
-	(interactive)
-	(find-file "~/.emacs.d/config.org"))
+  "Open Config Files"
+  (interactive)
+  (find-file "~/.emacs.d/config.org"))
 
 (defun config-reload ()
-	"Reload Config Files"
-	(interactive)
-	(org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+  "Reload Config Files"
+  (interactive)
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
 ;; Buffers
 (defun kill-current-buffer ()
@@ -35,11 +35,11 @@
   (kill-buffer (current-buffer)))
 
 (defun kill-all-buffers ()
-   (interactive)
-   (mapc 'kill-buffer (buffer-list)))
+  (interactive)
+  (mapc 'kill-buffer (buffer-list)))
 
 ;; Email
-;"rm $(notmuch search --output=files --exclude=false tag:deleted)"
+                                        ;"rm $(notmuch search --output=files --exclude=false tag:deleted)"
 (defun notmuch-delete-tagged ()
   "Delete emails tagged with deleted"
   (interactive)
@@ -48,3 +48,17 @@
   (message "%s Emails removed" (shell-command "wc ./email_list -l"))
   (shell-command "rm email_list")
   (shell-command "notmuch new"))
+
+(defun elfeed-open ()
+  "Open and update elfeed"
+  (interactive)
+  (elfeed-update)
+  (elfeed))
+
+(defun indent-buffer ()
+  "Indents an entire buffer using the default intenting scheme."
+  (interactive)
+  (save-excursion
+    (delete-trailing-whitespace)
+    (indent-region (point-min) (point-max) nil)
+    (untabify (point-min) (point-max))))
