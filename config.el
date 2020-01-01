a820d97fb47921c11813f04ef2d421796eb59eb0
Reberti Carvalho Soares <6reberti6@gmail.com>
2020-01-01 16:21:28 -0300

    Autosave/backup configs

--- config.el
+++ config.el
@@ -13,13 +13,29 @@
 (setq-default indent-tabs-mode t)
 (setq tab-width 4) ; or any other preferred value
 
-;; Bakcup
-(setq backup-directory-alist `(("." . "~/.emacs.d/backup/")))
-(setq backup-by-copying t)
-(setq delete-old-versions t
-  kept-new-versions 6
-  kept-old-versions 2
-  version-control t)
+;; Backup
+(defvar --backup-directory (concat user-emacs-directory "backups"))
+(if (not (file-exists-p --backup-directory))
+    (make-directory --backup-directory t))
+(setq backup-directory-alist `(("." . ,--backup-directory)))
+(setq make-backup-files t		; backup of a file the first time it is saved.
+      backup-by-copying t		; don't clobber symlinks
+      version-control t			; version numbers for backup files
+      delete-old-versions t		; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6		; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9		; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t		; auto-save every buffer that visits a file
+      auto-save-timeout 20		; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 200		; number of keystrokes between auto-saves (default: 300)
+      )
+
+;; Auto-save
+(defvar --autosave-directory (concat user-emacs-directory "autosaves"))
+(if (not (file-exists-p --autosave-directory))
+    (make-directory --autosave-directory t))
+(setq auto-save-file-name-transforms
+  `((".*" ,--autosave-directory t)))
 
 ;; Local LISP
 (add-to-list 'load-path "~/.emacs.d/lisp/")
